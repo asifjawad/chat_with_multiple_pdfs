48749adcf26919f72b165be19d051fbf392dd0af
asifjawad <jawad.asif@studenti.unipd.it>
2024-01-11 12:25:29 +0100

    Get PDFs

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 from dotenv import load_dotenv
+from src.utils import get_pdf_text
+
+# from PyPDF2 import PdfReader
+
+
+
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Searching from PDF", page_icon=":books:")
 
     st.header("Chatting with several PDFs :")
@@ -10,8 +17,16 @@ def main():
 
     with st.sidebar:
         st.subheader("Your Documents")
-        st.file_uploader("Upload your PDFs here")
-        st.button("Upload")
+        pdf_docs = st.file_uploader("Upload your PDFs here", accept_multiple_files=True)
+        if st.button("Upload"):
+            with st.spinner("Uploading files"):
+                # get pdf
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                # get the chunks
+
+                # create vector store
 
 
 if __name__ == '__main__':
