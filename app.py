fb4294f2505abe1a43c0ac338faa074bd687106d
asifjawad <jawad.asif@studenti.unipd.it>
2024-01-16 15:52:31 +0100

    Getting Conversation

--- app.py
+++ app.py
@@ -1,17 +1,54 @@
 import streamlit as st
 from dotenv import load_dotenv
-from src.utils import get_pdf_text, get_text_chunks, get_vector_store
+from src.utils import get_pdf_text, get_text_chunks, get_vector_store, get_conversation_chain
+
+from components.html_temp import css, user_template, bot_template
+
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    # st.write(response)
+
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2== 0:
+            st.write(user_template.replace("{{MSG}}", message.content ), unsafe_allow_html=True)
+
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+
+
+
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Searching from PDF", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+
+ 
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Chatting with several PDFs :")
-    st.text_input("What you want to know from your uploaded pdfs")
+    user_question = st.text_input("What you want to know from your uploaded pdfs")
+
+    if user_question:
+        handle_user_input(user_question)
+
+    st.write(user_template.replace("{{MSG}}", "Hi, This is Jawad, You can ask any Question"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "hello"), unsafe_allow_html=True)
 
 
     with st.sidebar:
-        st.subheader("Your Documents")
+        st.subheader("Your Documents") 
         pdf_docs = st.file_uploader("Upload your PDFs here", accept_multiple_files=True)
         if st.button("Upload"):
             with st.spinner("Uploading files"):
@@ -26,6 +63,10 @@ def main():
                 # create vector store
                 vectors = get_vector_store(chunks_text)
 
+                #starting conversation
+                st.session_state.conversation = get_conversation_chain(vectors)
+
+    # st.session_state.conversation
 
 if __name__ == '__main__':
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,14 @@
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 
+import streamlit as st
 
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -33,3 +37,21 @@ def get_vector_store(chunks):
      )
     vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
     return vectorstore
+
+
+def get_conversation_chain(vectors):
+    llm= ChatOpenAI()
+    memory = ConversationBufferMemory(
+        memory_key='chat_history',
+        return_messages=True
+    )
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectors.as_retriever(),
+        memory=memory
+
+    )
+
+    return conversation_chain
+
+
